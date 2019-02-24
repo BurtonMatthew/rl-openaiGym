028a4294aa590635c95da22433246e52ee22a8c8
Matt Burton <burtonmatthewa@gmail.com>
2019-02-24 05:09:42 -0500

    Added in basic experience replay functionality

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+import collections
 import gym
+import random
+import tensorflow as tf
+
+# hyperParams
+minibatchSize = 32
+replayMemorySize = 100000 #million in experience replay paper
+
+# data
+replayMemory = collections.deque([], replayMemorySize) #dequeue has O(n/64) access, should be list, but this handles dropping old elements for free
+
+# simulate games
 env = gym.make('Pong-v0')
-env.reset()
-for _ in range(1000):
-    env.step(env.action_space.sample())
-    env.render('human')
-env.close()  # https://github.com/openai/gym/issues/893
+observation = env.reset()
+for _ in range(10000):
+    # pick an action
+    action = env.action_space.sample()
+    # simulate step, record history
+    nextObservation, reward, done, info = env.step(action)
+    replayMemory.append((observation, action, reward, nextObservation))
+    observation = nextObservation
+    if done:
+        observation = env.reset()
+env.close()
+
+# train
+random.sample(replayMemory, minibatchSize)
