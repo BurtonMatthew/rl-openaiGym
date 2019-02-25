cb8b7d95bebdeb45d3aedb09e8fb52bc9086db66
Matt Burton <burtonmatthewa@gmail.com>
2019-02-25 01:31:53 -0500

    Stacking frames, trying to apply inputs to the QNet

--- deepqnetwork.py
+++ deepqnetwork.py
@@ -1,12 +1,20 @@
+import numpy
 import tensorflow as tf
 
 class DeepQNetwork:
-    def __init__(self, numOutputs):
+    def __init__(self, stackSize, numOutputs):
         self.model = tf.keras.Sequential()
-        self.model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(8,8), strides=4, padding='valid', activation='relu', input_shape=(84,84,4))) #"rectifier nonlinearity"
-        self.model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(4,4), strides=2, padding='valid', activation='relu')) #"rectifier nonlinearity"
-        self.model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3,3), strides=1, padding='valid', activation='relu')) #"rectifier"
+        self.model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(8,8), strides=4, padding='valid', activation='relu', input_shape=[84,84,stackSize], dtype=tf.float32))
+        self.model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(4,4), strides=2, padding='valid', activation='relu'))
+        self.model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3,3), strides=1, padding='valid', activation='relu'))
         self.model.add(tf.keras.layers.Flatten())
         self.model.add(tf.keras.layers.Dense(512, activation='relu'))
-        self.model.add(tf.keras.layers.Dense(numOutputs, activation='softmax'))
-        #self.model.summary()
+        self.model.add(tf.keras.layers.Dense(numOutputs))
+        self.model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.RMSprop(lr=0.00025))
+        self.model.summary()
+
+    def apply(self, frameStack):
+        preparedStack = (numpy.stack(frameStack, axis=2) / 255.0).astype(numpy.float32)
+        print(preparedStack.shape)
+        print(preparedStack.dtype)
+        return self.model.apply(preparedStack)
--- main.py
+++ main.py
@@ -3,27 +3,52 @@ import deepqnetwork
 import gym
 import random
 
+from skimage import img_as_ubyte, io
+from skimage.transform import resize
+from skimage.color import rgb2gray
+
+# To draw images
+#io.imshow(ret)
+#io.show()
+
+def preProcessFrame(frame):
+    return img_as_ubyte(resize(rgb2gray(frame), output_shape=(84, 84), anti_aliasing=True, mode='constant'))
+
+def initEpisode(env, stackSize, noopAction):
+    stack = collections.deque([], stackSize)
+    stack.append(preProcessFrame(env.reset()))
+    for _ in range(stackSize-1):
+        stack.append(preProcessFrame(env.step(noopAction)[0]))
+    return stack
+
 # hyperParams
 minibatchSize = 32
 replayMemorySize = 100000 #million in experience replay paper
+initExploration = 1.0
+finalExploration = 0.1
+frameStackSize = 4
 
 # runtime vars
 env = gym.make('Pong-v0')
+noopAction = env.unwrapped.get_action_meanings().index("NOOP")
 replayMemory = collections.deque([], replayMemorySize) #dequeue has O(n/64) access, should be list, but this handles dropping old elements for free
-net = deepqnetwork.DeepQNetwork(env.action_space.n)
+qNetwork = deepqnetwork.DeepQNetwork(env.action_space.n, frameStackSize)
 
 # simulate games
-observation = env.reset()
-for _ in range(10000):
+frameStack = initEpisode(env, frameStackSize, noopAction)
+
+for _ in range(10):
     # pick an action
+    qNetwork.apply(frameStack)
     action = env.action_space.sample()
     # simulate step, record history
-    nextObservation, reward, done, info = env.step(action)
-    replayMemory.append((observation, action, reward, nextObservation))
-    observation = nextObservation
+    observation, reward, done, info = env.step(action)
+    oldStack = frameStack.copy()
+    frameStack.append(preProcessFrame(observation))
+    replayMemory.append((oldStack, action, reward, frameStack.copy()))
     if done:
-        observation = env.reset()
+        frameStack = initEpisode(env, frameStackSize, noopAction)
 env.close()
 
 # train
-random.sample(replayMemory, minibatchSize)
+#random.sample(replayMemory, minibatchSize)
