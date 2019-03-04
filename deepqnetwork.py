037aff26b1d82e25bbba8c50842e7f7a4f980dcd
Matt Burton <burtonmatthewa@gmail.com>
2019-03-03 21:19:51 -0500

    Bunch of small changes

--- deepqnetwork.py
+++ deepqnetwork.py
@@ -1,25 +1,12 @@
 import numpy
 import tensorflow as tf
 
-def dequeToStack(deq):
-    return (numpy.stack(deq, axis=2) / 255.0).astype(numpy.float32)
-
 class DeepQNetwork:
     def __init__(self, numOutputs, stackSize):
-        #self.model = tf.keras.Sequential()
-        #self.model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(8,8), strides=4, padding='valid', activation='relu', input_shape=[84,84,stackSize], dtype=tf.float32))
-        #self.model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(4,4), strides=2, padding='valid', activation='relu'))
-        #self.model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3,3), strides=1, padding='valid', activation='relu'))
-        #self.model.add(tf.keras.layers.Flatten())
-        #self.model.add(tf.keras.layers.Dense(512, activation='relu'))
-        #self.model.add(tf.keras.layers.Dense(numOutputs))
-        #self.model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.RMSprop(lr=0.00025))
-        #self.model.summary()
-
         #prep the inputs
         self.inputs = tf.placeholder(shape=[None,84,84,stackSize], dtype=tf.uint8)
         self.qys = tf.placeholder(shape=[None], dtype=tf.float32)
-        self.selectedActions = tf.placeholder(shape=[None], dtype=tf.int32)
+        self.selectedActions = tf.placeholder(shape=[None,numOutputs], dtype=tf.float32)
 
         floatInputs = tf.cast(self.inputs, dtype=tf.float32)
         normalizedInputs = tf.math.divide(floatInputs, 255)
@@ -47,7 +34,7 @@ class DeepQNetwork:
         self.outputs = tf.layers.dense(inputs=hidden, units=numOutputs)
 
         #training
-        q = tf.gather(params=self.outputs, indices=self.selectedActions)
+        q = tf.reduce_sum(tf.multiply(self.outputs, self.selectedActions), axis=1)
         loss = tf.reduce_mean(tf.square(self.qys - q))
         self.trainFn = tf.train.RMSPropOptimizer(0.00025).minimize(loss)
 
@@ -55,14 +42,12 @@ class DeepQNetwork:
         return session.run(self.outputs, feed_dict = { self.inputs: numpy.expand_dims(numpy.stack(frameStack, axis=2), axis=0) })
 
     def train(self, session, batch):
-        return 0
         xs = []
         ys = []
         for xObs, _, _, _, yObs in batch:
-            xs.append(dequeToStack(xObs))
-            ys.append(dequeToStack(yObs))
+            xs.append(numpy.stack(xObs, axis=2))
+            ys.append(numpy.stack(yObs, axis=2))
 
-    #    xPredicts = self.model.predict(numpy.stack(xs))
         yPredicts = session.run(self.outputs, feed_dict = { self.inputs: numpy.stack(ys) })
 
         idx = 0
@@ -73,7 +58,9 @@ class DeepQNetwork:
                 qys.append(reward)
             else:
                 qys.append(reward + .99 * max(yPredicts[idx]))
-            actions.append(action)
+            newAction = [0,0,0,0,0,0]
+            newAction[action] = 1
+            actions.append(newAction)
             idx += 1
 
         session.run(self.trainFn, feed_dict = {self.inputs: numpy.stack(xs), self.qys: numpy.stack(qys), self.selectedActions: numpy.stack(actions)})
